8f4a99d5a2f3b45f6cbab5762b69423dc11f9679
thomas <doomsthomas@gmail.com>
2026-02-12 02:34:25 +0100

    bilinear attention similarity?

--- src/components/attention.py
+++ src/components/attention.py
@@ -4,6 +4,7 @@ from quimb.tensor import Tensor, TensorNetwork
 import torch
 
 from src.components.base import Component
+from src.components.compose import pad
 
 
 class Rotary(Component):
@@ -26,15 +27,16 @@ class Rotary(Component):
         y = torch.cat((-b, a), dim=-1)
         return (x * self.cos_cached[:, :x.size(-3)]) + (y * self.sin_cached[:, :x.size(-3)])
     
-    def network(self, mod, **kwargs):
+    def network(self, mod, device, dtype):
         data = [[[[1, 0], [0, 1]], [[0, -1], [1, 0]]], [[[0, 1], [-1, 0]], [[1, 0], [0, 1]]]]
-        black = Tensor(torch.tensor(data, **kwargs), inds=[f'{mod}:iq', f'{mod}:ik', f'{mod}:2q', f'{mod}:2k'], tags=['#'])
+        inds = [f'{mod}:iq', f'{mod}:ik', f'{mod}:2q', f'{mod}:2k'] 
+        black = Tensor(torch.tensor(data, device=device, dtype=dtype), inds=inds, tags=['#'])
         
         emb = torch.stack([self.cos, self.sin], dim=-1)
-        q_rot = Tensor(emb, inds=['out:t', f'{mod}:h', f'{mod}:iq'], tags=['E'])
-        k_rot = Tensor(emb, inds=['in:s', f'{mod}:h', f'{mod}:ik'], tags=['E'])
+        q = Tensor(emb, inds=['out:s', f'{mod}:h', f'{mod}:iq'], tags=['E'])
+        k = Tensor(emb, inds=['in:s', f'{mod}:h', f'{mod}:ik'], tags=['E'])
         
-        return black & q_rot & k_rot
+        return black & q & k
 
     
 class Mask(Component):
@@ -49,15 +51,15 @@ class Mask(Component):
         
         self.mask = nn.Buffer(data[kind], persistent=False)
     
-    def forward(self, x):
+    def forward(self, x: Tensor):
         return x * self.mask[None, None, :x.size(-2), :x.size(-1)]
     
-    def network(self, inds=['out:t', 'in:s'], tag='M'):
-        return Tensor(self.mask.data, inds=inds, tags=[tag])
+    def network(self, tag='M'):
+        return Tensor(self.mask.data, inds=['out:s', 'in:s'], tags=[tag])
 
 class Attention(Component):
     """Attention replacement using a quadratic scoring function."""
-    def __init__(self, d_model: int, n_head: int, n_ctx: int, mask: str, scale: int = 1) -> None:
+    def __init__(self, d_model: int, n_head: int, n_ctx: int, mask: str, scale: int = 1, bias: bool = True) -> None:
         super().__init__()
         self.d_head = d_model // n_head
         self.n_head = n_head
@@ -68,17 +70,17 @@ class Attention(Component):
         self.rotary = Rotary(self.d_head, n_ctx)
         self.mask = Mask(n_ctx, mask)
         
-        self.q1 = nn.Linear(d_model, d_model, bias=False)
-        self.k1 = nn.Linear(d_model, d_model, bias=False)
-        self.q2 = nn.Linear(d_model, d_model, bias=False)
-        self.k2 = nn.Linear(d_model, d_model, bias=False)
+        self.q1 = nn.Linear(d_model, d_model, bias=bias)
+        self.k1 = nn.Linear(d_model, d_model, bias=bias)
+        self.q2 = nn.Linear(d_model, d_model, bias=bias)
+        self.k2 = nn.Linear(d_model, d_model, bias=bias)
         
         self.v = nn.Linear(d_model, d_model, bias=False)
         self.o = nn.Linear(d_model, d_model, bias=False)
     
-    def forward(self, x):
+    def forward(self, x: Tensor):
         q1, k1, q2, k2, v = [rearrange(op(x), '... (n_head d_head) -> ... n_head d_head', n_head=self.n_head) for op in [self.q1, self.k1, self.q2, self.k2, self.v]]
-        q1, k1, q2, k2 = self.rotary(self.norm(q1)), self.rotary(self.norm(k1)), self.rotary(self.norm(q2)), self.rotary(self.norm(k2))
+        q1, k1, q2, k2 = [self.rotary(t) for t in [q1, k1, q2, k2]]
         
         scores1 = einsum(q1, k1, "... seq_q n_head d_head, ... seq_k n_head d_head -> ... n_head seq_q seq_k")
         scores2 = einsum(q2, k2, "... seq_q n_head d_head, ... seq_k n_head d_head -> ... n_head seq_q seq_k")
@@ -86,44 +88,34 @@ class Attention(Component):
         
         z = einsum(pattern, v, "... n_head seq_q seq_k, ... seq_k n_head d_head -> ... seq_q n_head d_head")
         z = rearrange(z, '... seq n_head d_head -> ... seq (n_head d_head)')
-        return x + self.o(z) * self.scale
+        return (x * (1 - self.scale)) + self.o(z) * self.scale
+
+    def _like(self):
+        return dict(device=self.o.weight.device, dtype=self.o.weight.dtype)
+
+    def attention(self, q: Tensor, k: Tensor, mod: str):
+        """Creates the QK-circuit with rotary embeddings."""
+        r = self.rotary.network(mod, **self._like())
+        s = Tensor(torch.full((self.d_head // 2,), 1.0 / self.d_head, **self._like()), inds=[f'{mod}:h'], tags=['S'])
+
+        rename = {idx: f'{mod}:{idx}' for idx in ['2q', '2k', 'h', 'q', 'k']}
+        return r & q.reindex(rename) & k.reindex(rename) & s
 
     def network(self):
-        raise NotImplementedError("Attention is currently unimplemented.")
+        """Attention-only TN (without residual). No constant output dim."""
+        d, n, h = self.d_model, self.n_head, self.d_head
 
-    def make_attn(self, q, k, mod):
-        rot = self.rotary.network(mod, **self._like())
-        # s = Tensor(torch.tensor([1.0 / self.d_head], **self._like()), inds=[f'{mod}:h'], tags=['S'])
-        
-        rename = {idx: f'{mod}:{idx}' for idx in ['2q', '2k', 'h', 'q', 'k']}
-        network = rot & q.reindex(rename) & k.reindex(rename) & Tensor(self.norm.weight.pow(2), inds=[f'{mod}:h'], tags=['S'])
-        
-        network.add_at(mod.upper())
-        return network
+        o = Tensor(self.scale * self.o.weight.view(d, n, h), inds=['out:d', 'n', 'ov:h'], tags=['O'])
+        v = Tensor(pad(self.v.weight, self.v.bias, constant=False).view(n, h, d + 1), inds=['n', 'ov:h', 'in:d0'], tags=['V'])
 
-    def make_norm(self, g, mod):
-        # s = Tensor(torch.tensor([1.0 / self.d_model], **self._like()), inds=[f'{mod}:h'], tags=['S'])
-        return g.reindex({f'2{mod}': '2', mod: f'left:{mod}'}) & g.reindex({f'2{mod}': '2', mod: f'right:{mod}'})
+        q1 = Tensor(pad(self.q1.weight, self.q1.bias, constant=False).view(n, 2, h // 2, d + 1), inds=['n', '2q', 'h', 'q'], tags=['Q'])
+        k1 = Tensor(pad(self.k1.weight, self.k1.bias, constant=False).view(n, 2, h // 2, d + 1), inds=['n', '2k', 'h', 'k'], tags=['K'])
+        q2 = Tensor(pad(self.q2.weight, self.q2.bias, constant=False).view(n, 2, h // 2, d + 1), inds=['n', '2q', 'h', 'q'], tags=['Q'])
+        k2 = Tensor(pad(self.k2.weight, self.k2.bias, constant=False).view(n, 2, h // 2, d + 1), inds=['n', '2k', 'h', 'k'], tags=['K'])
 
-    # def network(self):
-    #     # Rename some key/query entries in the network and define the matrices
-    #     rename = {'left:k': 'in:d1', 'right:k': 'in:d2', 'left:q': 'in:d3', 'right:q': 'in:d4'}
+        left = self.attention(q1, k1, 'left')
+        right = self.attention(q2, k2, 'right')
+        mask = self.mask.network()
 
-    #     o = make_padded(self.o.weight.view(self.d_model, self.n_head, self.d_head), dim=0, inds=['out:d', 'n', 'ov:h'], tags=['O']) * self.scale
-        
-    #     v = make_padded(self.v.weight.view(self.n_head, self.d_head, self.d_model), self.v.bias, dim=-1, inds=['n', 'ov:h', 'in:d0'], tags=['V'])
-    #     q = make_padded(self.q.weight.view(self.n_head, 2, self.d_head // 2, self.d_model), self.q.bias, dim=-1, inds=['n', '2q', 'h', 'q'], tags=['Q'])
-    #     k = make_padded(self.k.weight.view(self.n_head, 2, self.d_head // 2, self.d_model), self.k.bias, dim=-1, inds=['n', '2k', 'h', 'k'], tags=['K'])
-
-    #     # Quadratic attention uses two identical key/query circuits
-    #     left, right = [self.make_attn(q, k, mod=mod) for mod in ['left', 'right']]
-    #     mask = self.mask.network()
-    #     layer = TensorNetwork([o, v, mask, left, right], check_collisions=False).reindex(rename)
-
-    #     # The residual connection consists two wires and a norm computation
-    #     res = make_wire(self.d_model, inds=['out:d', 'in:d0'], tags=["I"], **self._like())
-    #     connect = make_wire(self.n_ctx - 1, inds=['in:s', 'out:t'], tags=['M'], **self._like())
-    #     norm = self.make_norm(k, mod='k') & self.make_norm(q, mod='q')
-    #     residual = TensorNetwork([res, norm, connect], check_collisions=False).reindex(rename)
-    
-    #     return Diag(layer, residual, qkv=self.qkv)
+        rename = {'left:k': 'in:d1', 'right:k': 'in:d2', 'left:q': 'in:d3', 'right:q': 'in:d4'}
+        return TensorNetwork([o, v, mask, left, right], check_collisions=False).reindex(rename)
