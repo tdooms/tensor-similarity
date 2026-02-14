16a1662ebbfab8a04bddba6794c4c5a6a2f31124
melwina <melwina.albuquerque@flame.edu.in>
2026-02-14 20:30:06 +0530

    icl score

--- workspaces/mel/bilinear_attn/analysis/behaviour/__init__.py
+++ workspaces/mel/bilinear_attn/analysis/behaviour/__init__.py
@@ -2,6 +2,7 @@
 from .bigram import BigramAnalyzer
 from .ngram import NgramAnalyzer
 from .ablation import compute_ablated_loss, compute_val_loss, ablate_rotary
+from .icl import compute_icl_score
 from .tracker import BehaviourTracker, TrackerConfig
 from .plotting import plot_metrics, plot_all_metrics, load_metrics
 
@@ -11,6 +12,7 @@ __all__ = [
     "compute_ablated_loss",
     "compute_val_loss",
     "ablate_rotary",
+    "compute_icl_score",
     "BehaviourTracker",
     "TrackerConfig",
     "plot_metrics",
